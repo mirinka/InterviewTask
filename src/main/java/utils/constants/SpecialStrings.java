3610189a8de863bd61edbb3ac02220f1ad0153bc
mironic <mironic@abv.bg>
2023-05-24 20:36:41 +0300

    removed empty line

--- src/main/java/utils/constants/SpecialStrings.java
+++ src/main/java/utils/constants/SpecialStrings.java
@@ -9,7 +9,6 @@ public class SpecialStrings {
     public static final String EMOJI_STRING = "\uD83D\uDE06\uD83D\uDE43\uD83E\uDD73\uD83E\uDD29";
     public static final String SCRIPT_STRING = "<script>alert(\"test\")</script>";
     public static final String STRIPPED_SCRIPT_STRING = "-script-alert-test-script-";
-
     public static final String STRING_WITH_SPACES = "   test repository name   ";
     public static final String STRIPPED_SPACES_STRING = "-test-repository-name-";
 }
